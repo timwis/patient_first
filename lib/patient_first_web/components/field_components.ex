82bbc0313719b99e17d87484c1448ae0b8e8300e
timwis <tim@timwis.com>
2023-10-15 12:33:25 +0100

    Add remaining questions to clarking show view

--- lib/patient_first_web/components/field_components.ex
+++ lib/patient_first_web/components/field_components.ex
@@ -1,6 +1,8 @@
 defmodule PatientFirstWeb.FieldComponents do
   use Phoenix.Component
 
+  alias PatientFirstWeb.CoreComponents
+
   @datetime_format "%d/%m/%y %H:%M"
   @date_format "%d/%m/%y"
 
@@ -44,6 +46,60 @@ defmodule PatientFirstWeb.FieldComponents do
   def answer(%{value: %{"type" => "date"}} = assigns) do
     ~H"""
     <.date value={@value["date"]} />
+    """
+  end
+
+  def answer(assigns) do
+    ~H"""
+    <span>(Unsupported question type)</span>
+    """
+  end
+
+  @doc """
+  Renders an answer list.
+
+  ## Examples
+      <.answer_list questions={@questions} answers={@answers} />
+        <:field key="date_of_birth" />
+        <:field key="family_medical_conditions" />
+      </.answer_list>
+  """
+  attr :questions, :map, required: true
+  attr :answers, :map, required: true
+  attr :title, :string, default: ""
+
+  slot :field, required: true do
+    attr :key, :string
+    attr :title, :string
+  end
+
+  def answer_list(assigns) do
+    unknown_keys =
+      assigns.field
+      |> Enum.filter(fn %{key: key} ->
+        !Map.has_key?(assigns.questions, String.to_atom(key))
+      end)
+      |> Enum.map(fn field -> field.key end)
+
+    assigns = assign(assigns, :unknown_keys, unknown_keys)
+
+    ~H"""
+    <h2 :if={@title} class="lg font-semibold leading-8 text-zinc-800 mt-8">
+      <%= @title %>
+    </h2>
+    <ul :if={length(@unknown_keys) > 0}>
+      <h3>Unmatched question keys</h3>
+      <li :for={key <- @unknown_keys}><%= key %></li>
+    </ul>
+    <CoreComponents.list>
+      <:item
+        :for={field <- @field}
+        :if={@answers[String.to_atom(field.key)]}
+        title={Map.get(field, :title, @questions[String.to_atom(field.key)])}
+      >
+        <.answer value={@answers[String.to_atom(field.key)]} />
+      </:item>
+    </CoreComponents.list>
     """
   end
 end
